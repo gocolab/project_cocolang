c8bb2531afb141cbd991ab5443acd409b1a4e0f3
Sanghun Oh <otter.oh@gmail.com>
2024-02-04 21:52:20 +0900

    add somsthings

--- main.py
+++ main.py
@@ -26,19 +26,15 @@ from routes.events import event_router
 from routes.events_api import router as event_api_router
 from routes.users import router as user_router
 from routes.homes import router as home_router
-from routes.todo import router as todo_router
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
 app.include_router(event_api_router, prefix="/events_api")
 app.include_router(home_router, prefix="/home")
-app.include_router(todo_router, prefix="/todo")
-
 
 @app.on_event("startup")
 async def init_db():
     await settings.initialize_database()
-
 
 # @app.get("/")
 # async def home():
@@ -50,7 +46,7 @@ import os
 static_css_directory = os.path.join("resources", "css")
 static_images_directory = os.path.join("resources", "images")
 app.mount("/css", StaticFiles(directory=static_css_directory), name="static_css")
-# app.mount("/images", StaticFiles(directory=static_images_directory), name="static_images")
+app.mount("/images", StaticFiles(directory=static_images_directory), name="static_images")
 
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
